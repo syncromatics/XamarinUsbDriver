ef4e24cd998d030b2d8dba5055eb21a713f3a910
annymsMthd <joshuabnjmn@gmail.com>
2016-03-28 10:39:27 -0700

    finished up deployment scripts

--- build/tasks/nuget.rb
+++ build/tasks/nuget.rb
@@ -1,23 +1,32 @@
 nuspec :generate_nuspec do |nuspec|
     nuspec.id = "XamarinUsbDriver"
-    nuspec.version = "0.0.0"
+    nuspec.version = "#{get_build_num()}"
     nuspec.authors = ["syncromatics"]
-    nuspec.description = ""
+    nuspec.description = "Serial drivers for xamarin android"
     nuspec.output_file = @paths.driver_nuspec
     GetProjectDependencies(@paths.driver_folder).each do |dep|
         nuspec.dependency dep.Name, dep.Version
     end
-    nuspec.file @paths.driver_dll, "lib\\dnx451"
+    nuspec.file @paths.driver_dll, "lib\\MonoAndroid10"
 end
 
 desc "Pack nupkg to local artifacts folder"
 exec :pack_nuget, [:version] => [:default, :generate_nuspec] do |cmd, args|
   args.with_defaults(:version => get_build_num())
-  cmd.command = "/usr/bin/nuget"
+  cmd.command = "nuget"
   cmd.parameters = [
     "pack",
     "#{@paths.driver_nuspec}",
     "-OutputDirectory \"#{@paths.artifacts_nuget}\""]
+end
+
+exec :push_package, [:location, :password] do |cmd, args|
+  cmd.command = "nuget"
+  cmd.parameters = [
+    "push",
+    "\"#{@paths.artifacts_nuget}/XamarinUsbDriver.#{get_build_num()}.nupkg\"",
+    args.password,
+    "-Source \"#{args.location}\""]
 end
 
 class Dependency
--- src/XamarinUsbDriver/UsbSerial/FtdiSerialPort.cs
+++ src/XamarinUsbDriver/UsbSerial/FtdiSerialPort.cs
@@ -1,3 +1,35 @@
+/* Copyright 2011-2013 Google Inc.
+ * Copyright 2013 mike wakerly <opensource@hoho.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * Project home page: https://github.com/mik3y/usb-serial-for-android
+ *
+ * This driver is based on http://www.intra2net.com/en/developer/libftdi, and is
+ * copyright and subject to the following terms:
+ *
+ *   Copyright (C) 2003 by Intra2net AG
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License
+ *   version 2.1 as published by the Free Software Foundation;
+ *
+ *   opensource@intra2net.com
+ */
+
 using System;
 using System.IO;
 using System.Threading.Tasks;
