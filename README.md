87ffa8f5c03d7ec555f605071687aff223a06b90
Daniel James <daniel@thzinc.com>
2022-04-19 10:08:02 -0700

    EN-8616: Build and release via GitHub actions (#4)
    * (+semver: patch) Updated DX docs to latest template
    
    * (+semver: feature) Add GitHub Actions build config
    
    * (+semver: patch) Remove rake build scripts
    
    * (+semver: fix) Fixed Xamarin.Android library version
    
    * (+semver: fix) Fix munged solution file
    
    * (+semver: fix) Adding specific Nugetizer3000 package because there's a weird, old dependency in the VSfM template
    
    * (+semver: fix) Move gogitver to linux...
    
    * (+semver: fix) Was mislead by outdated GH Actions docs...
    
    * (+semver: fix) Use macOS build of gogitver 1.2.0 instead of gogitver-action
    
    * (+semver: fix) Do what gogitver-actions was doing and fetch everything
    
    * (+semver: patch) Set prerelease flag

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This is a .NET library to interact with Syncromatics' USB devices through Xamarin
 
+## Building
+
+[![GitHub Actions](https://img.shields.io/github/workflow/status/syncromatics/XamarinUsbDriver/build)](https://github.com/syncromatics/XamarinUsbDriver/actions/workflows/build.yml)
+[![NuGet](https://img.shields.io/nuget/v/XamarinUsbDriver.svg)](https://www.nuget.org/packages/XamarinUsbDriver/)
+[![NuGet Pre Release](https://img.shields.io/nuget/vpre/XamarinUsbDriver.svg)](https://www.nuget.org/packages/XamarinUsbDriver/)
+
+Requires a configured Xamarin environment to build.
+
 ## Code of Conduct
 
 We are committed to fostering an open and welcoming environment. Please read our [code of conduct](CODE_OF_CONDUCT.md) before participating in or contributing to this project.
@@ -17,4 +25,4 @@ We welcome contributions and collaboration on this project. Please read our [con
 [![license](https://img.shields.io/github/license/syncromatics/XamarinUsbDriver.svg)](https://github.com/syncromatics/XamarinUsbDriver/blob/master/LICENSE)
 [![GitHub contributors](https://img.shields.io/github/contributors/syncromatics/XamarinUsbDriver.svg)](https://github.com/syncromatics/XamarinUsbDriver/graphs/contributors)
 
-This software is made available by GMV Syncromatics Engineering under the  GNU LESSER GENERAL PUBLIC LICENSE.
+This software is made available by GMV Syncromatics Engineering under the GNU LESSER GENERAL PUBLIC LICENSE.
--- src/XamarinUsbDriver/Properties/AssemblyInfo.cs
+++ src/XamarinUsbDriver/Properties/AssemblyInfo.cs
@@ -3,28 +3,19 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("XamarinUsbDriver")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]// a.k.a. "Comments"
+#else
+[assembly: AssemblyConfiguration("Release")]// a.k.a. "Comments"
+#endif
+[assembly: AssemblyCompany("Syncromatics Corporation")]
 [assembly: AssemblyProduct("XamarinUsbDriver")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Syncromatics Corporation 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0-development")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
